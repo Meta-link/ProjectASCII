797ce23d8d7bdaca70553c59d03d1d18e616b772
Meta-link <Meta-link@hotmail.fr>
2015-11-04 20:33:29 +0100

    Player

---
+++ Player.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <vector>
+#include "Caractere.h"
+
+using namespace std;
+
+class Player
+{
+public:
+	Player(string _name);
+	void start();
+	void addUnit(Caractere* c);
+	bool moveUnit(int x, int y);
+	void nextUnit();
+	int getPm();
+	string getName();
+	vector<Caractere*> getCar();
+	int getUnitX();
+	int getUnitY();
+	~Player();
+
+private:
+	string name;
+	vector<Caractere*> units;
+	vector<Caractere*>::iterator it;
+};
+
